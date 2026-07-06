63ef543abf0d9c975992e82fbce50bd7d746c41b
Sohaib Arshid <sohaibali1277@gmail.com>
2026-07-06 13:45:41 +0500

    Add ApiError, ApiResponse, and AsyncHandler utility classes

---
+++ Apps/Api/src/utils/AsyncHandler.js
@@ -0,0 +1,8 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        peomise.resolved(requestHandler(req, res, next)
+            .catch((error) => next(error))
+        )
+    }
+}
+export default asyncHandler;
