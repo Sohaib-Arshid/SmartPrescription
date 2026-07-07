21994cf19df656d5fad6759860458d42567e6134
Sohaib Arshid <sohaibali1277@gmail.com>
2026-07-07 09:56:20 +0500

    Add user controller with token generation and registration functionality

---
+++ Apps/Api/src/modules/auth/controllers/user.controller.js
@@ -0,0 +1,22 @@
+import { asyncHandler } from "../utils/AsyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { User } from "../models/user.models"
+
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        return { refreshToken, accessToken }
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens")
+    }
+}
+
+const register = asyncHandler(async (req , res )=>{
+        
+})
