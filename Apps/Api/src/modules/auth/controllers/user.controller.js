236624ee3a4922e3f0be5ebda52bebcd11dcb536
Sohaib Arshid <sohaibali1277@gmail.com>
2026-07-07 10:14:33 +0500

    Refactor user registration and token generation logic for improved error handling

--- Apps/Api/src/modules/auth/controllers/user.controller.js
+++ Apps/Api/src/modules/auth/controllers/user.controller.js
@@ -6,6 +6,9 @@ import { User } from "../models/user.models"
 const generateAccessAndRefreshToken = async (userId) => {
     try {
         const user = await User.findById(userId)
+        if (!user) {
+            throw new ApiError(404, "User not found");
+        }
         const accessToken = user.generateAccessToken()
         const refreshToken = user.generateRefreshToken()
 
@@ -17,6 +20,33 @@ const generateAccessAndRefreshToken = async (userId) => {
     }
 }
 
-const register = asyncHandler(async (req , res )=>{
-        
+const register = asyncHandler(async (req, res) => {
+    const {email , name , password , role} = req.body
+
+    if(!email || !name || !password || !role){
+        throw new ApiError(403 , "these fields are required" )
+    }
+
+    const registerUser = await User.create({
+        name,
+        email,
+        password,
+        role
+    })
+
+    const existing = await User.findOne({ email })
+    if (existing) {
+        throw new ApiError(409, "User already exists");
+    }
+
+    const createdUser = await User.findById(registeredUser._id)
+        .select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, createdUser, "User registered successfully")
+    )
 })
