a06916f31776db93ff610e6847410dea969ceb42
Sohaib Arshid <sohaibali1277@gmail.com>
2026-07-07 17:41:54 +0500

    Add auth routes to the Express app

--- Apps/Api/src/app.js
+++ Apps/Api/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { router } from "./modules/auth/routes/auth.routes";
 
 const app = express();
 app.use(cors({
@@ -11,5 +12,7 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use("api/v1/auth" , authRouter);
 
 export { app };
